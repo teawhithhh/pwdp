909d0da7618ce5d95a9127bbb9011d803e3bac51
teawhithhh <125253179+teawhithhh@users.noreply.github.com>
2023-12-19 10:00:08 +0100

    Update pwdp.cxx

--- pwdp.cxx
+++ pwdp.cxx
@@ -10,13 +10,13 @@ int main(int argc, char* argv[])
 	{
 		fs::path cur_path(fs::current_path());
 		std::cout << "Path\n"
-							<< "----\n"
-							<< cur_path.string() << '\n';
+			  << "----\n"
+			  << cur_path.string() << '\n';
 	}	else if (argc == 2){
 		fs::path cur_argv_path{fs::absolute(argv[1])};
 		std::cout << "Path\n"
-							<< "----\n"
-							<< cur_argv_path.string() << '\n';
+			  << "----\n"
+			  << cur_argv_path.string() << '\n';
 	} else {
 		std::cerr << "TO MANY ARGUMENTS";
 		return -1;
