3bbbc2a4317aa2f8ef8365ce58ebe6f1cebb6f41
Palchikovskiy <nazar.palchikovskiy@live.vasteras.se>
2023-12-19 09:56:29 +0100

    pwdp first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# pwd - plus
+usage - 
+pwdp #current directory path
+pwdp ./ #relative path
+
+installation -
+put directory where exists pwdp to path
---
+++ pwdp.cxx
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[])
+{
+	
+	if (argc == 1)
+	{
+		fs::path cur_path(fs::current_path());
+		std::cout << "Path\n"
+							<< "----\n"
+							<< cur_path.string() << '\n';
+	}	else if (argc == 2){
+		fs::path cur_argv_path{fs::absolute(argv[1])};
+		std::cout << "Path\n"
+							<< "----\n"
+							<< cur_argv_path.string() << '\n';
+	} else {
+		std::cerr << "TO MANY ARGUMENTS";
+		return -1;
+	}
+}
